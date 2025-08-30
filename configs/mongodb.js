7e6ef6ceb8c9e27fe1d54b7e1e86b055dc1c1163
Priya <bhardwajpriya402@gmail.com>
2025-08-30 20:58:28 +0530

    first commit

---
+++ configs/mongodb.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}`, {
+      
+    });
+
+    mongoose.connection.on("connected", () => {
+      console.log(" Database Connected");
+    });
+
+    mongoose.connection.on("error", (err) => {
+      console.error(" Database connection error:", err);
+    });
+  } catch (err) {
+    console.error(" Failed to connect to database", err);
+    process.exit(1); // stop the server if DB fails
+  }
+};
+
+export default connectDB;
---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './configs/mongodb.js';
+import clerkWebhooks from './controllers/webhooks.js'
+
+
+const app = express();
+await connectDB();
+app.use(cors());
+
+app.get('/',(req,res)=>res.send("API Working"))
+
+app.post('/clerk',express.json(),clerkWebhooks)
+
+const  PORT  = process.env.PORT || 5000;
+
+app.listen(PORT,()=>console.log(`Server Running on PORT ${PORT}`));
+ 
